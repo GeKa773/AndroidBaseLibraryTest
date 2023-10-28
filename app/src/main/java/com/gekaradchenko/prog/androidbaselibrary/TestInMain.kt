3b678f178e763c9560fee2b13bac57a67fd744fb
GeKaR <GeKaRadchenko@gmail.com>
2023-10-28 23:49:50 +0300

    Initial commit

---
+++ app/src/main/java/com/gekaradchenko/prog/androidbaselibrary/TestInMain.kt
@@ -0,0 +1,5 @@
+package com.gekaradchenko.prog.androidbaselibrary
+
+class TestInMain(private val t: Int) {
+    fun getT(): Int = t
+}
---
+++ basenew/src/main/java/com/gekaradchenko/prog/basenew/bot/Test.kt
@@ -0,0 +1,5 @@
+package com.gekaradchenko.prog.basenew.bot
+
+class Test(private val a: Int) {
+    fun getA(): Int = a
+}
